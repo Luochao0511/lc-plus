caaa51cd5f744ebb8801c57d59bcd27be35485d5
骆超 <506398736@qq.com>
2024-02-15 21:55:22 +0800

    fix: 修改表格ui样式，修改工作流

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import locale from 'element-plus/es/locale/lang/zh-cn'
+import 'lc-plus/index.css'
 // 图标并进行全局注册
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
