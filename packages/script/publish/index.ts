6a33f7aabc152e540453073503487da3e9e3210d
骆超 <506398736@qq.com>
2024-02-16 16:08:27 +0800

    feat: 终极提示release-it

--- packages/script/publish/index.ts
+++
@@ -1,9 +0,0 @@
-import run from '../build/run.ts'
-import { rootPath } from '../build/path.ts'
-import { series } from 'gulp'
-
-export const publishComponent = async () => {
-  await run('release-it  --dry-run', `${rootPath}/dist`)
-}
-
-export default series(async () => publishComponent())
